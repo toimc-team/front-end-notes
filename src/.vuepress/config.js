99d46b2018f27acfaa64ed3ff4a81949f3ac10a8
brian <brian@toimc.com>
2021-04-18 23:46:43 +0800

    [doc]创建目录

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,28 +1,254 @@
-const path = require('path')
-const fs = require('fs')
-
-const resolve = (src) => path.join(__dirname, '../../', src)
+const { resolve, genSidebarConfig } = require('./utils/index')
 const { description } = require('../../package')
 
-const genSidebarConfig = (dir, { hasSub, exclude }) => {
-  const p = path.join(__dirname, '../', dir)
-  const files = fs.readdirSync(p)
-  const subDir = hasSub ? dir.split('/')[1] : ''
-  const arr = []
-  files.forEach(item => {
-    if (exclude.indexOf(item) !== -1) return
-    item = subDir ? subDir + '/' + path.basename(item, '.md') : path.basename(item, '.md')
-    arr.push(item)
-  })
-  arr.unshift(subDir + '/')
-  return arr
+const siderBarOptions = { hasSub: true, exclude: ['README.md', 'assets', '.DS_Store', 'docs', 'images'] }
+
+const nav = [
+  {
+    text: '首页',
+    link: '/'
+  },
+  {
+    text: '基础强化',
+    items: [
+      {
+        text: '环境搭建',
+        items: [
+          {
+            text: 'Vue',
+            link: '/basic/vue/'
+          },
+          {
+            text: 'Node.js',
+            link: '/basic/node/'
+          },
+          {
+            text: '调试技巧',
+            link: '/basic/debug/'
+          },
+          {
+            text: 'Docker',
+            link: '/basic/docker/'
+          }
+        ]
+      },
+      {
+        text: '语言基础',
+        items: [
+          {
+            text: 'TypeScript',
+            link: '/basic/ts/'
+          },
+          {
+            text: 'Dart',
+            link: '/basic/dart/'
+          }
+        ]
+      },
+      {
+        text: '数据库',
+        items: [
+          {
+            text: 'NoSQL',
+            link: '/basic/nosql/'
+          },
+          {
+            text: 'mongoDB',
+            link: '/basic/mongo/'
+          },
+          {
+            text: 'Redis',
+            link: '/basic/redis/'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    text: 'DevOps',
+    items: [
+      {
+        text: '效率工具',
+        items: [
+          {
+            text: 'Jenkins',
+            link: '/devops/jenkins/'
+          }
+        ]
+      },
+      {
+        text: '团队协同',
+        items: [
+          {
+            text: 'gitlab',
+            link: '/devops/gitlab/'
+          },
+          {
+            text: 'showDoc',
+            link: '/devops/showdoc/'
+          },
+          {
+            text: 'DOClever',
+            link: '/devops/doclever/'
+          }
+        ]
+      },
+      {
+        text: '容器化',
+        items: [
+          {
+            text: 'Docker进阶',
+            link: '/devops/docker-advance/'
+          },
+          {
+            text: 'Kubernetes',
+            link: '/devops/k8s/'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    text: '全端项目',
+    items: [
+      {
+        text: '社区项目',
+        items: [
+          {
+            text: '社区PC',
+            link: '/project/miniapp/'
+          },
+          {
+            text: '管理后台',
+            link: '/project/miniapp/'
+          },
+          {
+            text: 'WebApp',
+            link: '/project/miniapp/'
+          }
+        ]
+      },
+      {
+        text: '多平台+跨端',
+        items: [
+          {
+            text: '微信小程序',
+            link: '/project/miniapp/'
+          },
+          {
+            text: 'Flutter 2.0',
+            link: '/project/miniapp/'
+          },
+          {
+            text: 'Electron桌面端',
+            link: '/project/miniapp/'
+          }
+        ]
+      },
+      {
+        text: 'React世界',
+        items: [
+          {
+            text: 'React',
+            link: '/project/react/'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    text: '前端面试',
+    items: [
+      {
+        text: '笔试',
+        items: [
+          {
+            text: '分类笔试题',
+            link: '/interview/types/'
+          },
+          {
+            text: '笔记技巧',
+            link: '/interview/techs/'
+          }
+        ]
+      },
+      {
+        text: '面试技巧',
+        items: [
+          {
+            text: '技术面',
+            link: '/interview/basic/'
+          },
+          {
+            text: '项目面',
+            link: '/interview/projects/'
+          },
+          {
+            text: 'BOSS面',
+            link: '/interview/boss/'
+          },
+          {
+            text: 'HR面',
+            link: '/interview/hr/'
+          }
+        ]
+      },
+      {
+        text: '简历',
+        items: [
+          {
+            text: '写好简历',
+            link: '/interview/writor/'
+          },
+          {
+            text: '简历模板',
+            link: '/interview/templates/'
+          }
+        ]
+      },
+      {
+        text: '公司行业',
+        items: [
+          {
+            text: '选择公司',
+            link: '/interview/company/'
+          },
+          {
+            text: '行业洞悉',
+            link: '/interview/industry/'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    text: '更新日志',
+    link: '/update-logs/'
+  }
+]
+
+const sidebar = {
+  '/project/': [
+    {
+      title: '小程序',
+      collapsable: false,
+      children: genSidebarConfig('project/miniapp', siderBarOptions)
+    },
+    {
+      title: 'React',
+      collapsable: false,
+      children: [
+        'react/'
+      ]
+    }
+  ],
+  '/': [
+    {
+      title: '首页',
+      collapsable: false
+    }
+  ]
 }
-
-const siderBarOptions = { hasSub: true, exclude: ['README.md', 'assets', '.DS_Store', 'docs'] }
-
-// console.log(genSidebarConfig('project/hrsaas', siderBarOptions))
-// console.log(process.env.ITCAST === 'gitee' ? '/docs' : '/')
-// console.log(process.env.NODE_ENV)
 
 module.exports = {
   configureWebpack: {
@@ -67,57 +293,8 @@ module.exports = {
     docsDir: '',
     editLinkText: '',
     lastUpdated: false,
-    nav: [
-      {
-        text: '首页',
-        link: '/'
-      },
-      {
-        text: '大前端',
-        ariaLabel: '<test></test>',
-        items: [
-          {
-            text: '项目开发',
-            items: [
-              {
-                text: '小程序',
-                link: '/project/miniapp/'
-              },
-              {
-                text: 'React',
-                link: '/project/react/'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        text: '更新日志',
-        link: '/update-logs/'
-      }
-    ],
-    sidebar: {
-      '/project/': [
-        {
-          title: '小程序',
-          collapsable: false,
-          children: genSidebarConfig('project/miniapp', siderBarOptions)
-        },
-        {
-          title: 'React',
-          collapsable: false,
-          children: [
-            'react/'
-          ]
-        }
-      ],
-      '/': [
-        {
-          title: '首页',
-          collapsable: false
-        }
-      ]
-    }
+    nav: nav,
+    sidebar: sidebar
   },
 
   /**
---
+++ src/.vuepress/utils/index.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const fs = require('fs')
+
+exports.genSidebarConfig = (dir, { hasSub, exclude }) => {
+  const p = path.join(__dirname, '../../', dir)
+  const files = fs.readdirSync(p)
+  const subDir = hasSub ? dir.split('/')[1] : ''
+  const arr = []
+  files.forEach(item => {
+    if (exclude.indexOf(item) !== -1) return
+    item = subDir ? subDir + '/' + path.basename(item, '.md') : path.basename(item, '.md')
+    arr.push(item)
+  })
+  // console.log('🚀 ~ file: config.js ~ line 26 ~ genSidebarConfig ~ subDir', subDir)
+  arr.unshift(subDir + '/')
+  return arr
+}
+
+exports.resolve = (src) => path.join(__dirname, '../../../', src)
